2b3f39bff644e793e0ed349f0714b8a4c59e06b3
ranjan mehta <rnjnmhta@gmail.com>
2023-12-02 17:26:58 +0530

    final push

---
+++ app.js
@@ -0,0 +1,38 @@
+const bookTitle=document.getElementById("book-title")
+const bookAuthor=document.getElementById("book-author")
+const publicationYear=document.getElementById("publication-year")
+const addBookButton=document.getElementById("add-book-button")
+const bookList=document.getElementById("book-list")
+
+addBookButton.addEventListener("click",()=>{
+const Title= bookTitle.value
+const Author=bookAuthor.value
+const Year= publicationYear.value
+ if(!Title || !Author||  !Year  ){
+    alert("Fill the complete form")
+    return
+ }
+ const newBook = document.createElement("li");
+ newBook.className=`
+ d-flex justify-content-between alert alert-light
+ `
+newBook.innerHTML=`
+<strong>${Title} </strong>
+<p>by ${Author}, year:${Year}</p>
+<button id="remove-book" type="button" onclick=removeBook(event) class="btn btn-danger">Remove</button>
+`
+bookList.appendChild(newBook)// li put inside ul
+bookTitle.value=""
+bookAuthor.value=""
+publicationYear.value=""
+
+})
+
+
+
+function removeBook(event){
+const bookToBeRemoved=event.target.parentNode
+bookToBeRemoved.remove()
+
+
+}
